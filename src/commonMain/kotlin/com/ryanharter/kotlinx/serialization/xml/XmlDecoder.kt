9594d0b5068c2739bd78ce3444f44345adb1a2cc
Jake Wharton <jw@squareup.com>
2022-04-27 17:21:07 -0400

    Enable explicit API mode

--- src/commonMain/kotlin/com/ryanharter/kotlinx/serialization/xml/XmlDecoder.kt
+++ src/commonMain/kotlin/com/ryanharter/kotlinx/serialization/xml/XmlDecoder.kt
@@ -2,7 +2,7 @@ package com.ryanharter.kotlinx.serialization.xml
 
 import kotlinx.serialization.encoding.Decoder
 
-interface XmlDecoder : Decoder {
+public interface XmlDecoder : Decoder {
   public val xml: Xml
   public fun decodeXmlEntity(): XmlEntity
 }
